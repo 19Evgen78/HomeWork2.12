657925e5f085cab25197889d749ad2d4de781292
19kuz <19kuzminykh78@mail.ru>
2023-11-17 21:18:39 +0800

    ДЗ 2.12

---
+++ src/main/java/pro/sky/java/course2/basket/model/Basket.java
@@ -0,0 +1,24 @@
+package pro.sky.java.course2.basket.model;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@Component
+@SessionScope
+public class Basket {
+    private final Map<Integer,Integer> countByOrderId = new HashMap<>();
+    public List<Integer> add(List<Integer> ids) {
+        for (Integer i : ids) {
+            if(countByOrderId.containsKey(i)) {
+                countByOrderId.put(i,countByOrderId.get(i) + 1);
+            } else {
+                countByOrderId.put(i,1);
+            }
+        }
+        return ids;
+    }
+    public Map<Integer,Integer> get() {
+        return countByOrderId;
+    }
+}
---
+++ src/main/java/pro/sky/java/course2/basket/service/BasketService.java
@@ -0,0 +1,18 @@
+package pro.sky.java.course2.basket.service;
+import org.springframework.stereotype.Service;
+import pro.sky.java.course2.basket.model.Basket;
+import java.util.List;
+import java.util.Map;
+@Service
+public class BasketService {
+    private final Basket basket;
+    public BasketService(Basket basket) {
+        this.basket = basket;
+    }
+    public List<Integer> add(List<Integer> ids) {
+        return basket.add(ids);
+    }
+    public Map<Integer,Integer> get() {
+        return basket.get();
+    }
+}
